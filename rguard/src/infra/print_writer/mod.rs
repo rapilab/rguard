aa924937783d0d5c6c154c4af674f298fa1b367c
Phodal Huang <h@phodal.com>
2020-08-07 11:38:18 +0800

    feat: make compile success

--- rguard/src/infra/print_writer/mod.rs
+++ rguard/src/infra/print_writer/mod.rs
@@ -1,17 +1,18 @@
 pub struct PrintWriter {
-    out: Box<dyn Fn(&mut T)>
+    // out: Box<dyn Fn(&mut T)>
+    pub out: Box<()>
 }
 
 impl PrintWriter {
     pub fn new() -> PrintWriter {
         PrintWriter { out: Box::new(()) }
     }
-    pub fn new_fn<F>(self, mut f: F) -> A where F: FnMut(A)  {
-        let mut writer = PrintWriter {
-            out: Box::new(())
-        };
-
-        writer.out = f;
-        writer
-    }
+    // pub fn new_fn<F>(self, mut f: F) -> A where F: FnMut(A)  {
+    //     let mut writer = PrintWriter {
+    //         out: Box::new(())
+    //     };
+    //
+    //     writer.out = f;
+    //     writer
+    // }
 }
