f21ed1d7df181083ee22c09a53824af5324d755a
Phodal Huang <h@phodal.com>
2020-08-09 09:32:57 +0800

    feat: add lost method for class usage marker

--- rguard/src/shrink/class_usage_marker.rs
+++ rguard/src/shrink/class_usage_marker.rs
@@ -1,6 +1,13 @@
 use rguard_core::classfile::library_clazz::LibraryClazz;
 use rguard_core::classfile::program_class::ProgramClass;
 use rguard_core::classfile::visitor::class_visitor::ClassVisitor;
+use rguard_core::classfile::visitor::member_visitor::MemberVisitor;
+use rguard_core::classfile::program_member::ProgramMember;
+use rguard_core::classfile::program_method::ProgramMethod;
+use rguard_core::classfile::program_field::ProgramField;
+use rguard_core::classfile::attribute::attribute_visitor::AttributeVisitor;
+use rguard_core::classfile::attribute::attribute::Attribute;
+use rguard_core::classfile::clazz::Clazz;
 
 pub struct ClassUsageMarker {}
 
@@ -16,6 +23,26 @@ impl ClassVisitor for ClassUsageMarker {
     }
 
     fn visit_library_class(&self, library_clazz: LibraryClazz) {
+        unimplemented!()
+    }
+}
+
+impl MemberVisitor for ClassUsageMarker {
+    fn visit_program_member(self, program_class: ProgramClass, program_member: ProgramMember) {
+        unimplemented!()
+    }
+
+    fn visit_program_field(self, program_class: ProgramClass, program_field: ProgramField) {
+        unimplemented!()
+    }
+
+    fn visit_program_method(self, program_class: ProgramClass, program_method: ProgramMethod) {
+        unimplemented!()
+    }
+}
+
+impl AttributeVisitor for ClassVisitor {
+    fn visit_any_attribute(&self, clazz: Box<dyn Clazz>, attribute: Attribute) {
         unimplemented!()
     }
 }
